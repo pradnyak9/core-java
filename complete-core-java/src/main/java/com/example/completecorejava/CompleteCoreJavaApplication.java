2c627365e9817d601612a38d9f117e71a9812292
Pradnya Khairnar <pradnyakhairnar16@gmail.com>
2021-11-26 16:33:14 +0000

    Hello class added

--- complete-core-java/src/main/java/com/example/completecorejava/CompleteCoreJavaApplication.java
+++ complete-core-java/src/main/java/com/example/completecorejava/CompleteCoreJavaApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CompleteCoreJavaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world!");
 		SpringApplication.run(CompleteCoreJavaApplication.class, args);
+		Hello hello = new Hello("Pradnya");
+		hello.setName("PradnyaK");
+		System.out.println("Hello "+hello.getName()+", Welcome to gitpod!");
 	}
 
 }
---
+++ complete-core-java/src/main/java/com/example/completecorejava/Hello.java
@@ -0,0 +1,32 @@
+package com.example.completecorejava;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hello {
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello [name=" + name + "]";
+    }
+}
+
