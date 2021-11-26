4b9b7e722aaec558f5b3063af3cd16bc189255be
Pradnya Khairnar <pradnyakhairnar16@gmail.com>
2021-11-26 16:01:39 +0000

    Initial project setup of complete core java project

---
+++ complete-core-java/src/main/java/com/example/completecorejava/CompleteCoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.completecorejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteCoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteCoreJavaApplication.class, args);
+	}
+
+}
---
+++ complete-core-java/src/test/java/com/example/completecorejava/CompleteCoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completecorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteCoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
